0c8dcb31bb4ceaccf09d49ed0681175ac80321f4
Mohmed Dahy <mooodahy@gmail.com>
2026-02-15 17:25:16 +0200

    First commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import BuyCredit from './Pages/BuyCredit'
+import Result from './Pages/Result'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 
+min-h-screen bg-gradient-to-r from-white via-blue-50 to-blue-100 text-gray-800">
+
+      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/buy-credit' element={<BuyCredit />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { use } from 'react'
+import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+const Navbar = () => {
+
+    const [user,setUser] = React.useState(null);
+    const navigate = useNavigate();
+  return (
+    <div className='flex items-center justify-between py-4'>
+       <Link to='/'><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' /> </Link> 
+
+       <div>
+        {user ? 
+        <div className='text-sm sm:text-base lg:text-lg flex items-center gap-4'>          
+          <button className='flex items-center bg-gray-200 text-gray-800 px-3 py-1 rounded-lg hover:bg-gray-300 transition duration-300 cursor-pointer'>
+            <img src={assets.credit_star} alt="" className='w-5 h-5 ml-2' />
+            <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits Left : 50</p>
+          </button>
+         <p className='text-sm sm:text-base lg:text-lg'>Welcome, User!</p>
+         <div className='relative group'>
+            <img src={assets.profile_icon} className='w-10 drop-shadow' alt="" />
+            <div className='absolute top-12 right-0 bg-white shadow-lg rounded-lg p-4 hidden group-hover:block'>
+              <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'><li className='py-1 px-2 cursor-pointer pr-10'>Logout</li></ul>
+            </div>
+         </div>
+
+        </div>
+        :
+        <div className='flex items-center gap-4 text-sm sm:text-base lg:text-lg'>
+          <p   onClick={() => navigate('/buy-credit')} className='cursor-pointer hover:underline'>Pricing</p>
+          <button className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300'>Login</button>
+        </div>
+    }
+        
+        
+       </div>
+    </div>
+
+  )
+}
+
+export default Navbar
